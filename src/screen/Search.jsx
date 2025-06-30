64f967ab779a69e8f457a09e6c72165e8e7b2bbf
KhadeejaArshad <khadeejaarshad.metafroliclabs.com>
2025-06-30 06:23:36 -0700

    Fixed the search screen

--- src/screen/Search.jsx
+++ src/screen/Search.jsx
@@ -6,14 +6,30 @@ import Text from '../UI/SpText';
 
 const Search = () => {
   return (
-    <ScrollView style={styles.root} contentContainerStyle={{ alignItems: 'center', paddingTop:20 }}>
+    <ScrollView style={styles.root} contentContainerStyle={{marginHorizontal:50, paddingTop:20 }}>
         <View style={styles.search}>
              <TextInput placeholder='Search' placeholderTextColor='gray'
              style={{ fontFamily: fonts.regular, color: 'black' }}/>
              <Feather name="search" color='black' size={24} />
         </View>
-        <View>
+        <Text color='black' size={16}>Recent Searches</Text>
+        <View style={{flexDirection:'row', alignItems:'center',gap:10,marginVertical:8}}>
+                <Feather name="clock" color='black' size={16} />
+
             <Text size={16} color='black'>Spider Plant </Text>
+           
+             <View style={{transform:[{rotate:'45deg'}]}}>
+                <Feather name="plus" color='black' size={16} />
+             </View>
+        </View>
+         <View style={{flexDirection:'row', alignItems:'center', gap:10, marginVertical:8}}>
+                <Feather name="clock" color='black' size={16} />
+
+            <Text size={16} color='black'>Spider Plant </Text>
+           
+             <View style={{transform:[{rotate:'45deg'}]}}>
+                <Feather name="plus" color='black' size={16} />
+             </View>
         </View>
      
     </ScrollView>
@@ -33,6 +49,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-between',
         alignItems:'center',
+        marginVertical:10
         
        
     }
